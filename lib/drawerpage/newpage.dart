a539a2c0d4eef58cc612eaf1719f4369f3cf96fc
Htet Paing <htetpaing.github@gmail.com>
2019-11-24 22:59:09 +0630

    added drawer

---
+++ lib/drawerpage/newpage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class NewsPage extends StatefulWidget {
+  @override
+  _NewsPageState createState() => _NewsPageState();
+}
+
+class _NewsPageState extends State<NewsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text('News',style: TextStyle(
+            color: Colors.black
+        ),),
+      ),
+      body: Center(
+        child: Text('News Page'),
+      ),
+    );
+  }
+}
