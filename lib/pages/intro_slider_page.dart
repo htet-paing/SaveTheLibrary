dd7bda91369e3bc6b39ad4b06f992ba3de706643
Htet Myat Aung <htetmyat.hma65@gmail.com>
2019-12-04 15:40:16 +0630

    Intro Slider

---
+++ lib/pages/intro_slider_page.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+import 'package:intro_slider/dot_animation_enum.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:intro_slider/slide_object.dart';
+import 'package:save_the_library/home_page.dart';
+
+class MyIntroSlider extends StatefulWidget {
+  MyIntroSlider({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => MyIntroSliderState();
+
+
+}
+
+class MyIntroSliderState extends State<MyIntroSlider> {
+  List<Slide> slides = new List();
+
+  Function goToTab;
+
+  @override
+  void initState() { 
+    super.initState();
+
+    slides.add(
+      Slide(
+        title: "School",
+        styleTitle: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0,
+        ),
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",
+        styleDescription: TextStyle(
+          color: Colors.blueAccent,
+        ),
+      )
+    );
+
+    slides.add(
+      Slide(
+        title: "School1",
+        styleTitle: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0,
+        ),
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",
+        styleDescription: TextStyle(
+          color: Colors.blueAccent,
+        ),
+      ),
+    );
+
+    slides.add(
+      Slide(
+        title: "School2",
+        styleTitle: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0,
+        ),
+        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",
+        styleDescription: TextStyle(
+          color: Colors.blueAccent,
+        ),
+      ),
+    );
+  }
+
+  void onDonePress() {
+    // Back to the first tab
+    Navigator.pushNamed(context, '/homepage');
+  }
+
+  void onTabChangeCompleted(index) {
+    // Index of current tab is focused
+  }
+
+  Widget renderNextBtn() {
+    return Icon(
+      Icons.navigate_next,
+      color: Color(0xffffcc5c),
+      size: 35.0,
+    );
+  }
+
+  Widget renderDoneBtn() {
+    return Icon(
+      Icons.done,
+      color: Color(0xffffcc5c),
+    );
+  }
+
+  Widget renderSkipBtn() {
+    return Icon(
+      Icons.skip_next,
+      color: Color(0xffffcc5c),
+    );
+  }
+
+  List<Widget> renderListCustomTabs() {
+    List<Widget> tabs = new List();
+    for (int i = 0; i < slides.length; i++) {
+      Slide currentSlide = slides[i];
+      tabs.add(Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Container(
+          margin: EdgeInsets.only(bottom: 60.0, top: 60.0),
+          child: ListView(
+            children: <Widget>[
+              // GestureDetector(
+              //     child: Image.asset(
+              //   currentSlide.pathImage,
+              //   width: 200.0,
+              //   height: 200.0,
+              //   fit: BoxFit.contain,
+              // )),
+              Container(
+                child: Text(
+                  currentSlide.title,
+                  style: currentSlide.styleTitle,
+                  textAlign: TextAlign.center,
+                ),
+                margin: EdgeInsets.only(top: 20.0),
+              ),
+              Container(
+                child: Text(
+                  currentSlide.description,
+                  style: currentSlide.styleDescription,
+                  textAlign: TextAlign.center,
+                  maxLines: 5,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                margin: EdgeInsets.only(top: 20.0),
+              ),
+            ],
+          ),
+        ),
+      ));
+    }
+    return tabs;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new IntroSlider(
+      slides: this.slides,
+
+      // Skip button
+      renderSkipBtn: this.renderSkipBtn(),
+      colorSkipBtn: Color(0x33ffcc5c),
+      highlightColorSkipBtn: Color(0xffffcc5c),
+
+      // Next button
+      renderNextBtn: this.renderNextBtn(),
+
+      // Done button
+      renderDoneBtn: this.renderDoneBtn(),
+      onDonePress: this.onDonePress,
+      colorDoneBtn: Color(0x33ffcc5c),
+      highlightColorDoneBtn: Color(0xffffcc5c),
+
+      // Dot indicator
+      colorDot: Color(0xffffcc5c),
+      sizeDot: 13.0,
+      typeDotAnimation: dotSliderAnimation.SIZE_TRANSITION,
+
+      //Tabs
+      listCustomTabs: this.renderListCustomTabs(),
+      backgroundColorAllSlides: Colors.white,
+      refFuncGoToTab: (refFunc) {
+        this.goToTab = refFunc;
+      },
+
+      // Show or hide status bar
+      shouldHideStatusBar: true,
+
+      // On tab change completed
+      onTabChangeCompleted: this.onTabChangeCompleted,
+    );
+  }
+      
+        
+}
