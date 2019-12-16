2c4c0fdaa2ccfa26180c96040c375aaaa3404ed5
Htet Paing <htetpaing.github@gmail.com>
2019-12-16 19:08:23 +0630

    libraries detail

--- lib/pages/home/views/libraries/libraries_view.dart
+++ lib/pages/home/views/libraries/libraries_view.dart
@@ -1,4 +1,7 @@
+import 'package:chopper/chopper.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:save_the_library/pages/home/views/books/components/book_author_list.dart';
 import 'package:save_the_library/pages/home/views/view_widget.dart';
 import 'components/library_list.dart';
 
@@ -17,8 +20,30 @@ class LibrariesView extends StatefulWidget implements ViewWidget {
 }
 
 class _LibrariesViewState extends State<LibrariesView> {
+  List<Widget> _widgetList = [
+    LibraryList(key: ValueKey(typeOf<LibraryList>()),),
+    BookAuthorList(key: ValueKey(typeOf<BookAuthorList>()),)
+
+  ];
   @override
   Widget build(BuildContext context) {
-    return LibraryList();
+    return DefaultTabController(
+      length: _widgetList.length,
+      child: Scaffold(
+        appBar: TabBar(
+          labelColor: Colors.teal,
+          unselectedLabelColor: Colors.grey,
+          indicatorColor: Colors.teal,
+          tabs: [
+            Tab(icon: Icon(Icons.list)),
+            Tab(icon: Icon(Icons.place)),
+          ],
+        ),
+        body: TabBarView(
+          dragStartBehavior: DragStartBehavior.down,
+          children: _widgetList,
+        ),
+      ),
+    );
   }
 }
